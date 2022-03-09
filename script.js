c89ea6b165852effee3998b062b0d9dec193f465
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-03-09 22:57:17 +0530

    improved add function

--- script.js
+++ script.js
@@ -15,35 +15,19 @@ function add(){
     if(get(`name`) == ``) return alert(`Please enter a name`);
     if(parseInt(get(`rate`)).toString() == "NaN") return alert(`Please enter a valid number`);
     if(parseInt(get(`quantity`)).toString() == "NaN") return alert(`Please enter a valid number`);
-
-    let itemSrNo = document.createTextNode(document.getElementById(`list`).childElementCount);
-    let itemName = document.createTextNode(get(`name`));
-    let itemRate = document.createTextNode(get(`rate`));
-    let itemQuantity = document.createTextNode(get(`quantity`));
-    let itemTotal = document.createTextNode(get(`rate`)*get(`quantity`));
-
-    let itemSrNoElement = document.createElement(`td`);
-    let itemNameElement = document.createElement(`td`);
-    let itemRateElement = document.createElement(`td`);
-    let itemQuantityElement = document.createElement(`td`);
-    let itemTotalElement = document.createElement(`td`);
-
-    let itemRow = document.createElement(`tr`);
-
-    itemSrNoElement.appendChild(itemSrNo);
-    itemNameElement.appendChild(itemName);
-    itemRateElement.appendChild(itemRate);
-    itemQuantityElement.appendChild(itemQuantity);
-    itemTotalElement.appendChild(itemTotal);
-
-    itemRow.appendChild(itemSrNoElement);
-    itemRow.appendChild(itemNameElement);
-    itemRow.appendChild(itemRateElement);
-    itemRow.appendChild(itemQuantityElement);
-    itemRow.appendChild(itemTotalElement);
-
-    document.getElementById(`list`).appendChild(itemRow);
-
+    let itemSrNo = document.getElementById(`list`).childElementCount;
+    let itemName = get(`name`);
+    let itemRate = get(`rate`);
+    let itemQuantity = get(`quantity`);
+    let itemTotal = get(`rate`)*get(`quantity`)
+    let element = `<tr>
+        <td>${itemSrNo}</td>
+        <td>${itemName}</td>
+        <td>${itemRate}</td>
+        <td>${itemQuantity}</td>
+        <td>${itemTotal}</td>
+    </tr>`
+    $("#list").append(element);
     total += get(`rate`)*get(`quantity`);
-    document.getElementById(`total`).textContent = `Total Price: ${total}`;
+    $("#total").text(total);
 }
